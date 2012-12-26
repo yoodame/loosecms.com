6d5f3225d1e196e0eeba90e0214c145f9d8ca34e
yoodame <yaw@madhen.co>
2012-12-26 20:26:48 +0000

    test pull request

---
+++ github-post-receive.php
@@ -0,0 +1,7 @@
+<?php
+	error_reporting(E_ALL);
+	ini_set('display_errors','On'); // print errors on screen
+	
+	$file = fopen( '/home/ubuntu/git-pull-requests/thingstodobeforeidie.in', 'w+' );
+	fclose( $file );
+
