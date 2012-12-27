3d294a87f67b7ee551cc400486adfbd315804d8f
yoodame <yoodame@gmail.com>
2012-12-27 11:15:56 -0600

    test

--- github-post-receive.php
+++ github-post-receive.php
@@ -1,7 +1,26 @@
 <?php
-	error_reporting(E_ALL);
-	ini_set('display_errors','On'); // print errors on screen
+	// error_reporting(E_ALL);
+	// ini_set('display_errors','On'); // print errors on screen
 	
-	$file = fopen( '/home/ubuntu/git-pull-requests/thingstodobeforeidie.in', 'w+' );
-	fclose( $file );
+	// $file = fopen( '/home/ubuntu/git-pull-requests/thingstodobeforeidie.in', 'w+' );
+	// fclose( $file );
 
+
+try
+{
+  $payload = json_decode($_REQUEST['payload']);
+}
+catch(Exception $e)
+{
+  exit(0);
+}
+
+//log the request
+file_put_contents('logs/github.txt', print_r($payload, TRUE), FILE_APPEND);
+
+
+if ($payload->ref === 'refs/heads/master')
+{
+  // path to your site deployment script
+  exec('./build.sh');
+}
