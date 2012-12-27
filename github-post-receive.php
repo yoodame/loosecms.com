c89e071510166f42d9d3090d5fb4ed3ceb84fb03
yoodame <yaw@madhen.co>
2012-12-27 19:40:13 +0000

    test

--- github-post-receive.php
+++ github-post-receive.php
@@ -1,9 +1,11 @@
 <?php
-	// error_reporting(E_ALL);
-	// ini_set('display_errors','On'); // print errors on screen
+	 error_reporting(E_ALL);
+	 ini_set('display_errors','On'); // print errors on screen
 	
-	// $file = fopen( '/home/ubuntu/git-pull-requests/thingstodobeforeidie.in', 'w+' );
-	// fclose( $file );
+	exec('echo "new line" >> /home/ubuntu/git-pull-requests/loosecms.com'); 
+
+//	$file = fopen( '/home/ubuntu/git-pull-requests/loosecms.com', 'w+' );
+//	fclose( $file );
 
 
 // try
