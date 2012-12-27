3fbebd2c1caa5dfc2d96837ebd39c2331e4112fa
yoodame <yoodame@gmail.com>
2012-12-27 11:53:58 -0600

    test

--- github-post-receive.php
+++ github-post-receive.php
@@ -6,21 +6,21 @@
 	// fclose( $file );
 
 
-try
-{
-  $payload = json_decode($_REQUEST['payload']);
-}
-catch(Exception $e)
-{
-  exit(0);
-}
+// try
+// {
+//   $payload = json_decode($_REQUEST['payload']);
+// }
+// catch(Exception $e)
+// {
+//   exit(0);
+// }
 
-//log the request
-file_put_contents('/home/ubuntu/git-pull-requests/thingstodobeforeidie.in', print_r($payload, TRUE), FILE_APPEND);
+// //log the request
+// file_put_contents('/home/ubuntu/git-pull-requests/thingstodobeforeidie.in', print_r($payload, TRUE), FILE_APPEND);
 
 
-if ($payload->ref === 'refs/heads/master')
-{
-  // path to your site deployment script
-  //exec('./build.sh');
-}
+// if ($payload->ref === 'refs/heads/master')
+// {
+//   // path to your site deployment script
+//   //exec('./build.sh');
+// }
